463af445750eb67c36d34a84c4aa2dacd3ce23ba
ATUL SRIVASTAVA <atulsrivastava3@yahoo.com>
2018-11-06 18:59:54 -0500

    SERIAL WORKINGgit status

--- lab_3/sender.vhd
+++ lab_3/sender.vhd
@@ -19,11 +19,11 @@ architecture Behavioral of sender is
 
 
     -- NetID Array, counter i and constant n that is length of array.
-    type str is array (0 to 3) of std_logic_vector(7 downto 0);
-    signal netID        : str := (X"31", X"37", X"30", X"30");
+    type str is array (0 to 5) of std_logic_vector(7 downto 0);
+    signal netID        : str := (X"61", X"73", X"32", X"36", X"31", X"39");
     signal i            : STD_LOGIC_VECTOR (3 downto 0) := "0000";
-    constant n          : STD_LOGIC_VECTOR(3 downto 0) := "0100";
-    
+    constant n          : STD_LOGIC_VECTOR(3 downto 0) := "0110";
+     
     -- State variables and state declarations
     type state is (idle,busyA, busyB, busyC);
     signal curr      : state := idle;
